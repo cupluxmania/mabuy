1f1bb4c7b7d6d66e35d0099dd7ea9e5c055bfb5d
cupluxmania <dungutulen@gmail.com>
2026-03-12 11:57:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 const db = firebase.firestore();
 
 const halls=[
-{name:"Hall 5",start:5001,end:5078},
-{name:"Hall 6",start:6001,end:6189},
-{name:"Hall 7",start:7001,end:7185}
+{name:"Hall 5",start:5001,end:5020},
+{name:"Hall 6",start:6001,end:6020}
 ];
 
 const floor=document.getElementById("floor");
 
 function initFloor(){
 
-floor.innerHTML="";
-
 halls.forEach(h=>{
 
 const hall=document.createElement("div");
 hall.className="hall";
 
-const header=document.createElement("div");
-header.className="hallHeader";
-header.innerText=h.name;
+const title=document.createElement("h3");
+title.innerText=h.name;
 
 const grid=document.createElement("div");
 grid.className="grid";
@@ -35,29 +31,26 @@ booth.dataset.id=i;
 booth.dataset.status="available";
 booth.dataset.name="";
 
-booth.setAttribute("data-id",i);
-booth.setAttribute("data-name","");
-
-booth.onclick=()=>openBoothPopup(booth);
+booth.onclick=()=>openBooth(booth);
 
 grid.appendChild(booth);
 
 }
 
-hall.appendChild(header);
+hall.appendChild(title);
 hall.appendChild(grid);
 
 floor.appendChild(hall);
 
 });
 
-loadSavedBooths();
+loadBooths();
 
 }
 
 let currentBooth=null;
 
-function openBoothPopup(booth){
+function openBooth(booth){
 
 currentBooth=booth;
 
@@ -88,26 +81,16 @@ currentBooth.dataset.name=name;
 
 currentBooth.className="booth "+status;
 
-currentBooth.setAttribute("data-name",name);
-
-saveBoothData(currentBooth);
+db.collection("booths").doc(currentBooth.dataset.id).set({
+status:status,
+name:name
+});
 
 closeModal();
 
 };
 
-function saveBoothData(booth){
-
-db.collection("booths").doc(booth.dataset.id).set({
-
-status:booth.dataset.status,
-name:booth.dataset.name
-
-});
-
-}
-
-function loadSavedBooths(){
+function loadBooths(){
 
 db.collection("booths").get().then(snapshot=>{
 
@@ -122,8 +105,6 @@ booth.dataset.name=doc.data().name;
 
 booth.className="booth "+doc.data().status;
 
-booth.setAttribute("data-name",doc.data().name);
-
 });
 
 });
@@ -132,50 +113,32 @@ booth.setAttribute("data-name",doc.data().name);
 
 /* DRAG */
 
-const floorContainer=document.getElementById("floorContainer");
+const container=document.getElementById("floorContainer");
 
-let isDragging=false;
+let isDown=false;
 let startX,startY,scrollLeft,scrollTop;
 
-floorContainer.addEventListener("mousedown",e=>{
+container.addEventListener("mousedown",e=>{
 
-isDragging=true;
+isDown=true;
 
-startX=e.pageX-floorContainer.offsetLeft;
-startY=e.pageY-floorContainer.offsetTop;
+startX=e.pageX;
+startY=e.pageY;
 
-scrollLeft=floorContainer.scrollLeft;
-scrollTop=floorContainer.scrollTop;
-
-floorContainer.style.cursor="grabbing";
+scrollLeft=container.scrollLeft;
+scrollTop=container.scrollTop;
 
 });
 
-floorContainer.addEventListener("mouseup",()=>{
+container.addEventListener("mouseup",()=>isDown=false);
+container.addEventListener("mouseleave",()=>isDown=false);
 
-isDragging=false;
-floorContainer.style.cursor="grab";
+container.addEventListener("mousemove",e=>{
 
-});
+if(!isDown) return;
 
-floorContainer.addEventListener("mouseleave",()=>{
-
-isDragging=false;
-floorContainer.style.cursor="grab";
-
-});
-
-floorContainer.addEventListener("mousemove",e=>{
-
-if(!isDragging) return;
-
-e.preventDefault();
-
-const x=e.pageX-floorContainer.offsetLeft;
-const y=e.pageY-floorContainer.offsetTop;
-
-floorContainer.scrollLeft=scrollLeft-(x-startX);
-floorContainer.scrollTop=scrollTop-(y-startY);
+container.scrollLeft=scrollLeft-(e.pageX-startX);
+container.scrollTop=scrollTop-(e.pageY-startY);
 
 });
 
@@ -212,17 +175,14 @@ const data=[];
 document.querySelectorAll(".booth").forEach(b=>{
 
 data.push({
-
 Booth:b.dataset.id,
 Status:b.dataset.status,
-Exhibitor:b.dataset.name
-
+Name:b.dataset.name
 });
 
 });
 
 const ws=XLSX.utils.json_to_sheet(data);
-
 const wb=XLSX.utils.book_new();
 
 XLSX.utils.book_append_sheet(wb,ws,"Booths");
@@ -234,9 +194,7 @@ XLSX.writeFile(wb,"booths.xlsx");
 /* IMPORT */
 
 document.getElementById("uploadBtn").onclick=function(){
-
 document.getElementById("importFile").click();
-
 };
 
 document.getElementById("importFile").addEventListener("change",function(e){
@@ -248,7 +206,6 @@ const reader=new FileReader();
 reader.onload=function(evt){
 
 const wb=XLSX.read(evt.target.result,{type:"binary"});
-
 const ws=wb.Sheets[wb.SheetNames[0]];
 
 const data=XLSX.utils.sheet_to_json(ws);
@@ -260,13 +217,14 @@ const booth=document.querySelector(`[data-id='${row.Booth}']`);
 if(!booth) return;
 
 booth.dataset.status=row.Status;
-booth.dataset.name=row.Exhibitor;
+booth.dataset.name=row.Name;
 
 booth.className="booth "+row.Status;
 
-booth.setAttribute("data-name",row.Exhibitor);
-
-saveBoothData(booth);
+db.collection("booths").doc(row.Booth).set({
+status:row.Status,
+name:row.Name
+});
 
 });
 
